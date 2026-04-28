cda50447c94e298ed749b360b6a7de75283a0c80
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-27 21:21:09 -0500

    try/catch

--- En-Clase/04-lunes 27/01-fake-api.js
+++ En-Clase/04-lunes 27/01-fake-api.js
@@ -1,6 +1,22 @@
+
+
+// const API = "https://fakeapi.net/products";
+
+//     const response = await fetch (API);
+//     const data = await response.json ();
+
+//     console.log(data);
+
 const API = "https://fakeapi.net/products";
+async function getProducts() {
+    try{
+        const response = await fetch (API);
+        const data = await response.json();
 
-    const response = await fetch (API);
-    const data = await response.json ();
+        console.log(data);
+    } catch(error){
+        console.error("Error al conectarse al FakeApi");
+    }
+}
 
-    console.log(data);
+getProducts();
