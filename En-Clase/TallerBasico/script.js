4c47df8708295dfd1203cb5b54df95c790cf9ea6
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-29 21:42:13 -0500

    cajero

---
+++ En-Clase/TallerBasico/script.js
@@ -0,0 +1,27 @@
+// Parte 1: Funciones + Listas
+// Sumar elementos de un array:
+
+// Crea una función sumarArray(array) que reciba un array de números y devuelva la suma.
+// Ejemplo: sumarArray([1, 2, 3]) → 6.
+// Encontrar el número mayor:
+
+function sumarArray (){}
+
+
+// Función numeroMayor(array) que retorne el número más grande de un array.
+// Ejemplo: numeroMayor([5, 2, 9, 1]) → 9.
+// Contar elementos pares:
+
+// Función contarPares(array) que cuente cuántos números pares hay.
+// Ejemplo: contarPares([3, 8, 2, 10]) → 3.
+// Invertir un array:
+
+// Función invertirArray(array) que retorne un nuevo array invertido.
+// Ejemplo: invertirArray(["a", "b","c"]) → ["c", "b", "a"].
+// Buscar un elemento:
+
+// Función buscarElemento(array, elemento) que devuelva true si el elemento existe.
+// Ejemplo: buscarElemento(["rojo", "azul"], "azul") → true.
+
+
+
