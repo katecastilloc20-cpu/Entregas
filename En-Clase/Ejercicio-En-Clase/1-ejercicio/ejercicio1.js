cd54cfa4a9a5e8c00f2bdd0371119c7529cd47b1
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-30 19:15:44 -0500

    ejercicio 1 y 2

---
+++ En-Clase/Ejercicio-En-Clase/1-ejercicio/ejercicio1.js
@@ -0,0 +1,6 @@
+//Declara dos variables (nombre y edad) usando let y asígnales tu nombre y edad. Luego, muestra un mensaje concatenado con console.log: "Hola, mi nombre es [nombre] y tengo [edad] años".
+
+let nombre = 'luisa';
+let edad = 23;
+
+console.log ('Hola, mi nombre es', nombre , 'y tengo', edad, 'años');
---
+++ En-Clase/Ejercicio-En-Clase/2-ejercicio/ejer2.js
@@ -0,0 +1,10 @@
+//Crea una constante (PI) con el valor de 3.1416 y calcula el área de un círculo (área = π × radio²). Pide el radio con prompt y muestra el resultado con alert.
+
+
+const PI = 3.1416;
+
+let radio = Number(prompt ('ingresa el radio del circulo'));
+
+let area = PI * (radio * radio);
+
+alert ('el area del circulo es', area);
