ead2051cd9ad01e081425bcaf94ac2ca7d966d87
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-30 20:33:51 -0500

    ejer 10

---
+++ En-Clase/Ejercicio-En-Clase/10-ejercicio/ejer10.js
@@ -0,0 +1,12 @@
+//Combina dos arrays:
+// a. Crea array1 = [1, 2, 3] y
+// b. array2 = [4, 5, 6].
+// c. Únelos en un nuevo array (array3)
+// usando concat().
+
+const lista = [1,2,3]
+const lista1 = [4,5,6]
+
+const lista3 = lista.concat(lista1)
+
+console.log(lista3)
