3d1e4075c7f2ee388604e6ae32973f1a0849d124
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-30 20:27:23 -0500

    ejer 9

---
+++ En-Clase/Ejercicio-En-Clase/9-ejercicio/ejer9.js
@@ -0,0 +1,8 @@
+ //Pide al usuario 3 colores (usando prompt) y guárdalos en un array (colores). Luego muéstralos con console.log.
+
+
+let color = prompt('digita el color numero 1')
+let color1 = prompt('digita el color numero 2')
+let color2 = prompt('digita el color numero 3')
+
+console.log (`tus colores son: ${color}, ${color1}, ${color2}`)
