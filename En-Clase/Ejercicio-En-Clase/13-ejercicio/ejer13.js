85fd62b581589268842e425507888f0c5b35a631
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-30 20:55:44 -0500

    ejer 13

---
+++ En-Clase/Ejercicio-En-Clase/13-ejercicio/ejer13.js
@@ -0,0 +1,11 @@
+//Agrega una nueva propiedad (profesion) al objeto persona y muéstrala.
+
+let persona = {
+    nombre: 'carolina',
+    edad: 32,
+    ciudad: 'cali'
+};
+
+persona.profesion = 'doctor'
+
+console.log(persona);
