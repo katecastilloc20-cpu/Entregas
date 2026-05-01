88c71e4505ce3b0e06fb012c46494e59405a393f
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-30 20:18:11 -0500

    ejer 8

---
+++ En-Clase/Ejercicio-En-Clase/7-ejercicio/ejer7.js
@@ -0,0 +1,7 @@
+//Agrega una fruta al final del array usando push() y muestra el array completo.
+
+let frutas = ['mango','fresa','melon'];
+
+frutas.push = ('mandarina');
+
+console.log (frutas);
---
+++ En-Clase/Ejercicio-En-Clase/8-ejercicio/ejer8.js
@@ -0,0 +1,7 @@
+//Elimina el primer elemento del array usando shift() y muestra el array modificado.
+
+let frutas = ['mango','fresa','melon'];
+
+frutas.shift ();
+
+console.log (frutas);
