7cb846cc469257aa7279f73d89ea8c129541a4b5
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-30 19:58:16 -0500

    ejercicio 6

--- En-Clase/Ejercicio-En-Clase/6-ejercicio/ejer6.js
+++ En-Clase/Ejercicio-En-Clase/6-ejercicio/ejer6.js
@@ -1,3 +1,5 @@
 //Crea una lista (frutas) con 3 frutas. Muestra la segunda fruta usando su índice.
 
-let frutas = { 'mango';'fresa';'melon'}
+let frutas = ['mango','fresa','melon']
+
+console.log (frutas[1])
