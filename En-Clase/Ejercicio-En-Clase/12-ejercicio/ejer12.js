25d3e82a6438abef9b29a1be522c2527aac18b03
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-30 20:47:45 -0500

    ejer 12

---
+++ En-Clase/Ejercicio-En-Clase/12-ejercicio/ejer12.js
@@ -0,0 +1,13 @@
+//Modifica una propiedad: Cambia la ciudad de persona y muestra el objeto actualizado.
+
+let persona = {
+    nombre: 'carolina',
+    edad: 32,
+    ciudad: 'cali'
+}
+
+persona.ciudad= 'bogota'
+
+console.log(persona)
+
+
