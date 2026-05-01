b02ace75ac30169fadc91f7dd7880c003fb020df
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-30 20:41:56 -0500

    ejer 11

---
+++ En-Clase/Ejercicio-En-Clase/11-ejercicio/ejer11.js
@@ -0,0 +1,13 @@
+//Crea un objeto (persona) con propiedades: nombre, edad, ciudad. Muestra cada propiedad usando notación de punto (persona.nombre) y notación de corchetes (persona['edad']).
+
+
+let persona = {
+    nombre: 'carolina',
+    edad: 32,
+    ciudad: 'cali'
+
+}
+
+console.log(persona.nombre)
+console.log(persona['edad'])
+console.log(persona.ciudad)
