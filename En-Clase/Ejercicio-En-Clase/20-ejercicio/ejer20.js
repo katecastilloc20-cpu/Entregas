aef5d7a8f108ce6ad8118165f3fd90e958ed442c
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-05-04 18:51:53 -0500

    ejer 20

---
+++ En-Clase/Ejercicio-En-Clase/20-ejercicio/ejer20.js
@@ -0,0 +1,12 @@
+// Operador ternario:
+// Pide al usuario un número y muestra "Par" o
+// "Impar" usando un operador ternario.
+
+
+let numero = Number(prompt('escribe un número'))
+
+if (numero % 2 === 0){
+    console.log ('El número es par')
+} else {
+    console.log ('El número es impar')
+}
