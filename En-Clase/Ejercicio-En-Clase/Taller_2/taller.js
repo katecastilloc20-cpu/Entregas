4798d2d044f0f366604ff2ca2621d9e7e4b87cd8
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-05-07 21:50:45 -0500

    ejercicio 1

---
+++ En-Clase/Ejercicio-En-Clase/Taller_2/taller.js
@@ -0,0 +1,19 @@
+//Crea una función paresEnRango(inicio, fin) que devuelva un array con todos los números pares entre inicio y fin (ambos incluidos si corresponden).
+
+
+function paresEnRango (inicio,fin){
+    let lista = []
+    for (let i = inicio; i <= fin; i++) {
+        if(i % 2 == 0){
+            lista.push(i)
+        }
+
+        
+        
+        
+    }
+
+    console.log(lista)
+}
+
+paresEnRango (1,10)
